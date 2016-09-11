421e05d0f672ecfa8b7f1e1872ede381968daaa9
Jan-Luca Pfeiffer <janluca@gmail.com>
2016-09-11 22:03:42 +0200

    Complete Authentication System with Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910201608) do
+ActiveRecord::Schema.define(version: 20160911193859) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20160910201608) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
